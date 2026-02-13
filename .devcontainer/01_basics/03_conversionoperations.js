b78f8d8ff55979e2dbfc98908f8968d65d9c708b
Shubha NB <shubhanb28@gmail.com>
2026-02-13 09:36:53 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionoperations.js
@@ -0,0 +1,30 @@
+let score = "shubha"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+console.log (typeof valueInNumber)
+console.log (valueInNumber)
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+//null=>0
+//undefined=>NaN
+
+let isLoggedIn="shubha"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+
+
+//true=>1 false=>0
+//""=>false
+//"shubha"=true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
