a379408a13cec2492e511e698bd74b9127563acb
Shubha NB <shubhanb28@gmail.com>
2026-02-13 07:39:37 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId=123456
+let accountEmail="shubhanb28@gmail.com"
+var accounPassword="1122"
+accountCity="benglore"
+let accountState;
+//accountId=2
+
+accountEmail="shub23@gmail.com"
+accounPassword="1234"
+accountCity="mumbai"
+console.log(accountId);
+
+/*
+prefer not to use var 
+because of issue in blockscope and function scope
+*/
+console.table([accountId,accountEmail,accounPassword,accountCity,accountState])
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"usestrict";//treat alljs code as newer version
+
+//alert(3+3) //we are using nodejs,not browser
+
+console.log(3
+    +
+    3)//code readability  should be high
+
+
+    let name="shubha"
+    let age=18
+    let isLoggedIn=false
+    //number=>2topower53
+    //bigint
+    //string=> true/false
+    //null=>standalone value
+    //undefined=> variable is declared but not assigned
+    //symbol=> unique identifier
+
+    console.log(typeof age)
+    console.log(typeof isLoggedIn)
+    console.log(typeof name)
+    console.log(typeof null) 
